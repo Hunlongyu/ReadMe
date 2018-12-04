7662688f4536321649ce909a4c74313b9361f5d4
Hunlongyu <hunlongyu@gmail.com>
2018-12-04 09:29:26 +0800

    Auto stash before merge of "master" and "origin/master"

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,6 +7,8 @@ import router from './router'
 import 'highlight.js/styles/github.css'
 import hljs from 'highlight.js'
 import './helper/extenal_link.js'
+import Notifications from 'vue-notification'
+Vue.use(Notifications)
 
 import Notifications from 'vue-notification'
 Vue.use(Notifications)
