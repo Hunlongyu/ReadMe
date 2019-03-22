a6073b088b644816e6b57a6ac57c513dff4e75a2
hunlongyu <hunlongyu@gmail.com>
2019-03-22 13:30:13 +0800

    进一步学习该数据库插件的语法、功能

--- src/components/add.vue
+++ src/components/add.vue
@@ -2,18 +2,29 @@
   <div class="add">add</div>
 </template>
 <script>
-// import db from '../database/db'
+import db from '../database/db'
 export default {
   name: 'add',
   created () {
     // db.md.put({
-    //   repository: 'Hunlongyu/ReadMe-client',
+    //   repository: 'longyu',
     //   tag: 'js',
     //   url: 'https://raw.githubusercontent.com/Hunlongyu/ReadMe-client/dev/README.md'
-    // }).then((md) => {
-    //   console.log(db.md.repository)
-    // }).catch((error) => {
-    //   console.log(error)
+    // })
+
+    // db.md.delete(16);
+    db.md.where('repository').startsWithIgnoreCase('hun').toArray(e => {
+      console.log(e)
+    })
+    // db.md.get(24).then(e => {
+    //   console.log(e.tag)
+    // })
+    // db.md.limit(10).each(e => {
+    //   console.log(e.id)
+    // })
+
+    // db.md.orderBy(':id').limit(10).each(e => {
+    //   console.log(e.tag)
     // })
   }
 }
