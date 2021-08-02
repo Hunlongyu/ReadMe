cf9f9902916495d106ebe386cd3f05961ffa8e62
Hunlongyu <hunlongyu@gmail.com>
2021-08-02 18:21:18 +0800

    feat: login 界面完善

--- src/renderer/views/login/App.vue
+++ src/renderer/views/login/App.vue
@@ -5,45 +5,21 @@
       <div class="left"></div>
       <div class="right">
         <div class="box nt-box">
-          <button class="button nt-box" @click="authLogin">Sign up width Github</button>
-          <span class="line nt-line" data-title="or"></span>
-          <button class="button nt-box">Sign up width Github name</button>
+          <button class="button nt-btn" @click="authLogin">Sign up width Github</button>
+          <span class="line nt-line" data-title=""></span>
+          <p>1. 登录后可同步用户信息和设置</p>
+          <p>2. 登录后可直接添加或取消收藏</p>
         </div>
       </div>
     </div>
-    <!-- <a href="https://github.com/login/oauth/authorize?client_id=dce5a448c5e9cca4d566&redirect_uri=http://localhost:8080/#/home">授权</a>
-    <a href="http://localhost:8080/#/home">home</a> -->
   </div>
 </template>
 <script lang="ts" setup>
-import { onMounted } from 'vue'
-import { useRouter } from 'vue-router'
 import Frame from '../../components/Frame.vue'
-
-const router = useRouter()
-
-// 获取连接参数
-function getQueryString (param: string) {
-  var reg = new RegExp('(^|&)' + param + '=([^&]*)(&|$)')
-  var r = window.location.search.substr(1).match(reg)
-  if (r != null) return unescape(r[2])
-  return ''
-}
-
-function checkCode () {
-  const link = getQueryString('code')
-  if (link !== '') {
-    router.push({ name: 'Home' })
-  }
-}
 
 function authLogin () {
   window.location.href = 'https://github.com/login/oauth/authorize?client_id=dce5a448c5e9cca4d566&redirect_uri=http://localhost:8080/#/home'
 }
-
-onMounted(() => {
-  // checkCode()
-})
 </script>
 <style lang="scss" scoped>
 .login{
@@ -68,12 +44,16 @@ onMounted(() => {
       .box{
         width: 320px;
         height: 480px;
+        p{
+          font-size: 14px;
+        }
       }
       .button{
         padding: 10px 25px;
       }
       .line{
         width: 80%;
+        margin: 40px auto 26px;
       }
     }
   }
