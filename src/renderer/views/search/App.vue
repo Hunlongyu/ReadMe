488c98efbf43b21aac600879bbaf86cf2e0645e1
Hunlongyu <hunlongyu@gmail.com>
2021-08-15 23:23:28 +0800

    feat: 搜索

--- src/renderer/views/search/App.vue
+++ src/renderer/views/search/App.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="search">
-    <div class="filter">{{ $t('search.filter') }}</div>
+    <div class="filter">
+      <div class="type">
+        <span>Repositories</span>
+        <span>Code</span>
+        <span>Commits</span>
+        <span>Issues</span>
+        <span>Discussions</span>
+        <span>Users</span>
+      </div>
+      <div class="advanced">
+        <div class="item">
+          <span>With this many stars</span>
+          <el-input></el-input>
+        </div>
+        <div class="item">
+          <span>With this many forks</span>
+          <el-input></el-input>
+        </div>
+      </div>
+      <div class="language">
+        <span>javascript</span>
+      </div>
+    </div>
     <div class="content">{{ $t('search.content') }}</div>
   </div>
 </template>
