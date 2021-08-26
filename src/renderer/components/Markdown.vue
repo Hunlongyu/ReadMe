83abeb3db10a9cf0981d65ac865adddeddb5da30
Hunlongyu <hunlongyu@gmail.com>
2021-08-26 13:48:56 +0800

    fix: star chart active state

--- src/renderer/components/Markdown.vue
+++ src/renderer/components/Markdown.vue
@@ -8,7 +8,7 @@
         <span class="icon-btn" title="Github" @click="githubSite">
           <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M29.3444 30.4767C31.7481 29.9771 33.9292 29.1109 35.6247 27.8393C38.5202 25.6677 40 22.3137 40 19C40 16.6754 39.1187 14.5051 37.5929 12.6669C36.7427 11.6426 39.2295 4.00001 37.02 5.02931C34.8105 6.05861 31.5708 8.33691 29.8726 7.8341C28.0545 7.29577 26.0733 7.00001 24 7.00001C22.1992 7.00001 20.4679 7.22313 18.8526 7.63452C16.5046 8.23249 14.2591 6.00001 12 5.02931C9.74086 4.05861 10.9736 11.9633 10.3026 12.7946C8.84119 14.6052 8 16.7289 8 19C8 22.3137 9.79086 25.6677 12.6863 27.8393C14.6151 29.2858 17.034 30.2077 19.7401 30.6621" stroke="#333" stroke-width="2" stroke-linecap="round"/><path d="M19.7402 30.662C18.5817 31.9372 18.0024 33.148 18.0024 34.2946C18.0024 35.4411 18.0024 38.3465 18.0024 43.0108" stroke="#333" stroke-width="2" stroke-linecap="round"/><path d="M29.3443 30.4767C30.4421 31.9175 30.991 33.2112 30.991 34.3577C30.991 35.5043 30.991 38.3886 30.991 43.0108" stroke="#333" stroke-width="2" stroke-linecap="round"/><path d="M6 31.2156C6.89887 31.3255 7.56554 31.7388 8 32.4555C8.65169 33.5304 11.0742 37.5181 13.8251 37.5181C15.6591 37.5181 17.0515 37.5181 18.0024 37.5181" stroke="#333" stroke-width="2" stroke-linecap="round"/></svg>
         </span>
-        <span class="icon-btn" title="StarChart" @click="starChartEvent">
+        <span :class="['icon-btn', chartShow ? 'active' : '']" title="StarChart" @click="starChartEvent">
           <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M6 6V42H42" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M14 34L22 18L32 27L42 6" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
         </span>
         <span :class="['icon-btn', starred ? 'starred' : '']" title="Star" @click="starClickEvent">
@@ -297,6 +297,10 @@ defineExpose({
     margin-left: 12px;
     cursor: pointer;
     &:hover{
+      background-color: #f8f8f8;
+      border: 1px solid #d9e3e5;
+    }
+    &.active{
       background-color: #f8f8f8;
       border: 1px solid #d9e3e5;
     }
