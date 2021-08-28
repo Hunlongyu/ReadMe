8e7bd79d82eb0cb9a2008fcdbc61971b6c1ff18d
Hunlongyu <hunlongyu@gmail.com>
2021-08-28 22:11:59 +0800

    feat: add code component

---
+++ src/renderer/components/Code.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="code">
+    <div class="code-tree"></div>
+    <div class="code-content"></div>
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style lang="scss">
+
+</style>
