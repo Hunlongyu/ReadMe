c49393493d899648761fed331001fe4098de24f1
Hunlongyu <hunlongyu@gmail.com>
2021-09-02 08:52:25 +0800

    perf: 优化 user 模块

--- src/renderer/components/User.vue
+++ src/renderer/components/User.vue
@@ -6,9 +6,6 @@
           <span class="icon-btn" title="Github" @click="goGithub">
             <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="48" height="48" fill="white" fill-opacity="0.01"/><path d="M29.3444 30.4767C31.7481 29.9771 33.9292 29.1109 35.6247 27.8393C38.5202 25.6677 40 22.3137 40 19C40 16.6754 39.1187 14.5051 37.5929 12.6669C36.7427 11.6426 39.2295 4.00001 37.02 5.02931C34.8105 6.05861 31.5708 8.33691 29.8726 7.8341C28.0545 7.29577 26.0733 7.00001 24 7.00001C22.1992 7.00001 20.4679 7.22313 18.8526 7.63452C16.5046 8.23249 14.2591 6.00001 12 5.02931C9.74086 4.05861 10.9736 11.9633 10.3026 12.7946C8.84119 14.6052 8 16.7289 8 19C8 22.3137 9.79086 25.6677 12.6863 27.8393C14.6151 29.2858 17.034 30.2077 19.7401 30.6621" stroke="#333" stroke-width="2" stroke-linecap="round"/><path d="M19.7402 30.662C18.5817 31.9372 18.0024 33.148 18.0024 34.2946C18.0024 35.4411 18.0024 38.3465 18.0024 43.0108" stroke="#333" stroke-width="2" stroke-linecap="round"/><path d="M29.3443 30.4767C30.4421 31.9175 30.991 33.2112 30.991 34.3577C30.991 35.5043 30.991 38.3886 30.991 43.0108" stroke="#333" stroke-width="2" stroke-linecap="round"/><path d="M6 31.2156C6.89887 31.3255 7.56554 31.7388 8 32.4555C8.65169 33.5304 11.0742 37.5181 13.8251 37.5181C15.6591 37.5181 17.0515 37.5181 18.0024 37.5181" stroke="#333" stroke-width="2" stroke-linecap="round"/></svg>
           </span>
-          <span class="icon-btn" title="Export image!" @click="exportImage">
-            <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M39 6H9C7.34315 6 6 7.34315 6 9V39C6 40.6569 7.34315 42 9 42H39C40.6569 42 42 40.6569 42 39V9C42 7.34315 40.6569 6 39 6Z" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M18 23C20.7614 23 23 20.7614 23 18C23 15.2386 20.7614 13 18 13C15.2386 13 13 15.2386 13 18C13 20.7614 15.2386 23 18 23Z" fill="none" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M42 36L31 26L21 35L14 29L6 35" stroke="#333" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>
-          </span>
         </div>
       </div>
       <div class="user-content scroll" v-loading="loading">
@@ -52,6 +49,10 @@ async function init (form: string, u: SearchUsers | PublicUser) {
   if (form === 'db') {
     const info = u as PublicUser
     user.value = info
+    const header = document.querySelector('.user-header') as HTMLElement
+    if (header) {
+      header.style.marginTop = '0px'
+    }
   } else {
     const info = u as SearchUsers
     user.value = info
--- src/renderer/utils/user.ts
+++ src/renderer/utils/user.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Octokit } from '@octokit/core'
 import type { PublicUser } from '@/types'
+import { SearchUsers } from './search'
 
 // 获取用户 token
 async function getUserToken (code: string): Promise<string> {
@@ -19,6 +20,36 @@ async function getUserInfo (token: string): Promise<PublicUser> {
 async function getUserInfoNoToken (name: string): Promise<string> {
   const res = await axios.get(`https://api.github.com/users/${name}`, { headers: { Accept: 'application/vnd.github.v3.star+json' } })
   return res.data || ''
+}
+
+interface mdType {
+  name: string
+  content: string
+}
+
+async function getUserMD (user: PublicUser | SearchUsers) {
+  const login = user.login
+  const md = ['README.md', 'readme.md', 'README.MD', 'Readme.md', 'readme.MD', 'ReadMe.md', 'ReadMe.Md', 'ReadMe.MD', 'README.markdown', 'readme.markdown', 'README', 'readme', 'readMe.md', 'README.textile', 'README.rst']
+  let idx = 0
+  async function getMd (): Promise<mdType> {
+    const url = `https://raw.githubusercontent.com/${login}/${login}/master/${md[idx]}`
+    try {
+      const res = await axios.get(url)
+      return { name: md[idx], content: res.data }
+    } catch (error) {
+      idx++
+      if (idx < md.length) {
+        const res = await getMd()
+        if (res) return res
+        return { name: md[idx], content: '' }
+      } else {
+        return { name: md[idx], content: '' }
+      }
+    }
+  }
+  const result = await getMd()
+  if (result.name && result.content) return result
+  return { name: '', content: '' }
 }
 
 export {
