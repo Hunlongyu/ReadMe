43df1608d14efece33b094091bdcb0ecc7311d83
Hunlongyu <hunlongyu@gmail.com>
2021-09-04 17:00:02 +0800

    release: 发布测试版本: v2.0.14

--- src/renderer/utils/user.ts
+++ src/renderer/utils/user.ts
@@ -4,14 +4,8 @@ import type { PublicUser } from '@/types'
 
 // 获取用户 token
 async function getUserToken (code: string): Promise<string> {
-  const isDev = process.env.NODE_ENV !== 'production'
-  if (isDev) {
-    const res = await axios.post('https://github.com/login/oauth/access_token', { code, client_id: process.env.VUE_APP_clientId, client_secret: process.env.VUE_APP_clientSecret }, { headers: { Accept: 'application/vnd.github.v3.star+json' } })
-    return res.data.access_token
-  } else {
-    const res = await axios.post('https://github.com/login/oauth/access_token', { code, client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET }, { headers: { Accept: 'application/vnd.github.v3.star+json' } })
-    return res.data.access_token
-  }
+  const res = await axios.post('https://github.com/login/oauth/access_token', { code, client_id: process.env.VUE_APP_clientId, client_secret: process.env.VUE_APP_clientSecret }, { headers: { Accept: 'application/vnd.github.v3.star+json' } })
+  return res.data.access_token
 }
 
 // 获取用户信息，使用 token
